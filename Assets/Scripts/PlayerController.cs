c0b4150bc60812ca02f7c10ae8aa2ddd25d6bdb0
LeonGu0402 <2653579853@qq.com>
2024-11-15 21:04:38 -0500

    Task 123

--- Assets/Scripts/PlayerController.cs
+++ Assets/Scripts/PlayerController.cs
@@ -1,3 +1,4 @@
+using System.Security.Cryptography.X509Certificates;
 using UnityEngine;
 
 public class PlayerController : MonoBehaviour
@@ -6,11 +7,21 @@ public class PlayerController : MonoBehaviour
     {
         left, right
     }
+    public float speed;
+    public float maxSpeed;
+    public float acceleration;
+    public float deceleration;
+    public Transform groundCheck;
+    public LayerMask ground;
+    private Rigidbody2D rb;
+    private float horizontalMove;
+    private bool isGround;
+
 
     // Start is called before the first frame update
     void Start()
     {
-        
+        rb = GetComponent<Rigidbody2D>();
     }
 
     // Update is called once per frame
@@ -19,26 +30,66 @@ public class PlayerController : MonoBehaviour
         // The input from the player needs to be determined and
         // then passed in the to the MovementUpdate which should
         // manage the actual movement of the character.
-        Vector2 playerInput = new Vector2();
+
+        horizontalMove = Input.GetAxisRaw("Horizontal");
+        Vector2 playerInput = new Vector2(horizontalMove, 0);
+
+
         MovementUpdate(playerInput);
     }
 
     private void MovementUpdate(Vector2 playerInput)
     {
+        rb.velocity = new Vector2(playerInput.x * speed, 0);
+        
+        if (horizontalMove > 0 || horizontalMove < 0)
+        {
+            speed += acceleration * Time.deltaTime;
+            if (speed > maxSpeed)
+            {
+                speed = maxSpeed;
+            }
+        }
+
+        if (horizontalMove == 0)
+        {
+            speed -= deceleration * Time.deltaTime;
+            if (speed < 0)
+            {
+                speed = 0;
+            }
+        }
 
     }
 
     public bool IsWalking()
     {
-        return false;
+        if (horizontalMove ==0)
+        {
+            return false;
+        }
+        else
+        {
+            return true;
+        }
     }
     public bool IsGrounded()
     {
-        return false;
+        isGround = Physics2D.OverlapCircle(groundCheck.position, 0.2f, ground);
+
+        return isGround;
     }
 
     public FacingDirection GetFacingDirection()
     {
-        return FacingDirection.left;
+        float faceDirection = horizontalMove;
+        if (faceDirection < 0)
+        {
+            return FacingDirection.left;
+        }
+        else
+        {
+            return FacingDirection.right;
+        }
     }
 }
