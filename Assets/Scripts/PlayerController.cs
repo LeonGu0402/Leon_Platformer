7c330cfe94bdaf51d23d5c87fddd5c6da8b13497
LeonGu0402 <2653579853@qq.com>
2024-12-01 16:02:07 -0500

    Set Up
    set up for assignment, updated the horizontal and vertical movement

--- Assets/Scripts/PlayerController.cs
+++ Assets/Scripts/PlayerController.cs
@@ -4,33 +4,40 @@ using UnityEngine;
 
 public class PlayerController : MonoBehaviour
 {
+    [SerializeField] private Rigidbody2D rb;
     public enum FacingDirection
     {
         left, right
     }
-    public Transform groundCheck;
-    public LayerMask ground;
+    //public Transform groundCheck;
+
+    [Header("Horizontal")]
     public float maxSpeed;
     public float accelerationTime;
     public float decelerationTime;
+
+    [Header("Vertical")]
     public float apexHeight;
     public float apexTime;
     public float terminalSpeed;
     public float coyoteTime;
 
-
-
-
-    private Rigidbody2D rb;
-    private float horizontalMove;
+    [Header("Ground Check")]
     public bool isGround;
-    private bool isJump;
+    public float groundCheckOffset = 0.5f;
+    public Vector2 groundCheckSize = new Vector2(0.4f, 0.1f);
+    public LayerMask groundCheckMask;
+
+
+    private Vector2 playerInput;
+    private FacingDirection currentdirection = FacingDirection.right;
     private float acceleration;
     private float deceleration;
-    private Vector2 currentVelocity;
-    private float maxSpeedSqr;
+    private Vector2 velocity;
+    //private float maxSpeedSqr;
+    //private bool isJump;
     private float Gravity;
-    private float jumpVelocity;
+    private float jumpSpeed;
     private float airTime;
     private float fallingTimer;
 
@@ -38,30 +45,46 @@ public class PlayerController : MonoBehaviour
     void Start()
     {
         rb = GetComponent<Rigidbody2D>();
-        acceleration = maxSpeed / accelerationTime;
-        deceleration = maxSpeed / decelerationTime;
-        maxSpeedSqr = maxSpeed * maxSpeed;
+
+        //maxSpeedSqr = maxSpeed * maxSpeed;
     }
 
     void Update()
     {
-        // The input from the player needs to be determined and
-        // then passed in the to the MovementUpdate which should
-        // manage the actual movement of the character.
-        horizontalMove = Input.GetAxisRaw("Horizontal");
+        //horizontalMove = Input.GetAxisRaw("Horizontal");
         //Vector2 playerInput = new Vector2(horizontalMove, 0);
         //MovementUpdate(playerInput);
-
-        Jumping();
         //Debug.Log(rb.velocity);
-        Debug.Log(fallingTimer);
+        //Debug.Log(fallingTimer);
+        CheckForGround();
 
+        acceleration = maxSpeed / accelerationTime;
+        deceleration = maxSpeed / decelerationTime;
+        Gravity = -2 * apexHeight / (apexTime * apexTime);
+        jumpSpeed = 2 * apexHeight / apexTime;
+
+        rb.gravityScale = 0;
+
+        playerInput.x = Input.GetAxisRaw("Horizontal");
+
+        MovementUpdate(playerInput);
+        JumpUpdate();
+        //applying gravity
+        if (!isGround)
+        {
+            velocity.y += Gravity * Time.deltaTime;
+        }
+        else
+        {
+            velocity.y = 0;
+        }
+
+        rb.velocity = velocity;
 
     }
 
     private void FixedUpdate()
     {
-        MovementUpdate();
     }
 
 
@@ -91,96 +114,145 @@ public class PlayerController : MonoBehaviour
 
     //}
 
-
     //new Movement function
-    private void MovementUpdate()
+    //private void MovementUpdate()
+    //{
+    //    Vector2 moveDirection = Vector2.zero;
+
+    //    if (Input.GetKey(KeyCode.A))
+    //    {
+    //        moveDirection += Vector2.left;
+    //    }
+    //    if (Input.GetKey(KeyCode.D))
+    //    {
+    //        moveDirection += Vector2.right;
+    //    }
+
+    //    if (moveDirection.sqrMagnitude > 0)
+    //    {
+    //        velocity += Time.deltaTime * acceleration * moveDirection;
+    //        if (velocity.sqrMagnitude > maxSpeedSqr)
+    //        {
+    //            velocity = velocity.normalized * maxSpeed;
+    //        }
+    //    }
+    //    else
+    //    {
+    //        Vector2 velocityDelta = Time.deltaTime * deceleration * velocity.normalized;
+    //        if (velocityDelta.sqrMagnitude > velocity.sqrMagnitude)
+    //        {
+    //            velocity = Vector2.zero;
+    //        }
+    //        else
+    //        {
+    //            velocity -= velocityDelta;
+    //        }
+    //    }
+    //    rb.position += velocity * Time.deltaTime;
+    //}
+
+
+    //latest movement
+    private void MovementUpdate(Vector2 playerInput)
     {
-        Vector2 moveDirection = Vector2.zero;
-
-        if (Input.GetKey(KeyCode.A))
+        //facing direction
+        if (playerInput.x < 0)
         {
-            moveDirection += Vector2.left;
+            currentdirection = FacingDirection.left;
         }
-        if (Input.GetKey(KeyCode.D))
+        else if (playerInput.x > 0)
         {
-            moveDirection += Vector2.right;
+            currentdirection = FacingDirection.right;
         }
 
-        if (moveDirection.sqrMagnitude > 0)
+        if (playerInput.x != 0)
         {
-            currentVelocity += Time.deltaTime * acceleration * moveDirection;
-            if (currentVelocity.sqrMagnitude > maxSpeedSqr)
-            {
-                currentVelocity = currentVelocity.normalized * maxSpeed;
-            }
+            //accelerate
+            velocity.x += acceleration * playerInput.x * Time.deltaTime;
+            velocity.x = Mathf.Clamp(velocity.x, -maxSpeed, maxSpeed);
         }
         else
         {
-            Vector2 velocityDelta = Time.deltaTime * deceleration * currentVelocity.normalized;
-            if (velocityDelta.sqrMagnitude > currentVelocity.sqrMagnitude)
+            //decelerate
+            if (velocity.x > 0)
             {
-                currentVelocity = Vector2.zero;
+                velocity.x -= deceleration * Time.deltaTime;
+                velocity.x = Mathf.Max(velocity.x, 0);
             }
-            else
+            else if (velocity.x < 0)
             {
-                currentVelocity -= velocityDelta;
+                velocity.x += deceleration * Time.deltaTime;
+                velocity.x = Mathf.Min(velocity.x, 0);
             }
         }
-
-        rb.position += currentVelocity * Time.deltaTime;
 
     }
 
 
-    private void Jumping()
+    //old jump
+    //private void Jumping()
+    //{
+    //    Gravity = -2 * apexHeight / (apexTime * apexTime);
+    //    jumpVelocity = 2 * apexHeight / apexTime;
+
+
+
+    //    if (isGround && Input.GetKeyDown(KeyCode.Space))
+    //    {
+    //        airTime = 0;
+    //        isJump = true;
+    //    }
+
+    //    //turn off rb gravity
+    //    if (isGround)
+    //    {
+    //        rb.gravityScale = 1;
+    //        fallingTimer = 0;
+    //    }
+    //    else
+    //    {
+    //        rb.gravityScale = 0;
+    //        fallingTimer += Time.deltaTime;
+    //    }
+
+    //    if (isJump)
+    //    {
+
+    //        if (fallingTimer < coyoteTime && Input.GetKeyDown(KeyCode.Space))
+    //        {
+    //            airTime = 0;
+    //        }
+
+    //        airTime += Time.deltaTime;
+    //        rb.velocity = new Vector2(rb.velocity.x, Gravity * airTime + jumpVelocity);
+
+    //        //clamp fallling speed
+    //        if (rb.velocity.y < -terminalSpeed)
+    //        {
+    //            rb.velocity = new Vector2(rb.velocity.x, -terminalSpeed);
+    //        }
+    //    }
+    //}
+
+
+    //new jump
+    private void JumpUpdate()
     {
-        Gravity = -2 * apexHeight / (apexTime * apexTime);
-        jumpVelocity = 2 * apexHeight / apexTime;
-        
-
-
-        if (isGround && Input.GetKeyDown(KeyCode.Space))
+        if ((isGround) && Input.GetButton("Jump"))
         {
-            airTime = 0;
-            isJump = true;
+            velocity.y = jumpSpeed;
+            isGround = false;
         }
 
-        //turn off rb gravity
-        if (isGround)
-        {
-            rb.gravityScale = 1;
-            fallingTimer = 0;
-        }
-        else
-        {
-            rb.gravityScale = 0;
-            fallingTimer += Time.deltaTime;
-        }
-
-        if (isJump)
-        {
-
-            if (fallingTimer < coyoteTime && Input.GetKeyDown(KeyCode.Space))
-            {
-                airTime = 0;
-            }
-
-           airTime += Time.deltaTime;
-            rb.velocity = new Vector2(rb.velocity.x, Gravity * airTime + jumpVelocity);
-
-            //clamp fallling speed
-            if (rb.velocity.y < -terminalSpeed)
-            {
-                rb.velocity = new Vector2(rb.velocity.x, -terminalSpeed);
-            }
-        }
-
-
+        //terminal falling speed
+        velocity.y = Mathf.Max(velocity.y, -terminalSpeed);
 
     }
+
+
     public bool IsWalking()
     {
-        if (horizontalMove ==0)
+        if (playerInput.x ==0)
         {
             return false;
         }
@@ -191,28 +263,20 @@ public class PlayerController : MonoBehaviour
     }
 
 
+    private void CheckForGround()
+    {
+        isGround = Physics2D.OverlapBox(transform.position + Vector3.down * groundCheckOffset, groundCheckSize, 0, groundCheckMask);
+    }
     public bool IsGrounded()
     {
-        isGround = Physics2D.OverlapCircle(groundCheck.position, 0.2f, ground);
-
-        
-
+        //isGround = Physics2D.OverlapCircle(groundCheck.position, 0.2f, ground);
         return isGround;
     }
 
 
     public FacingDirection GetFacingDirection()
     {
-        float faceDirection = horizontalMove;
-        if (faceDirection < 0)
-        {
-            return FacingDirection.left;
-        }
-        else
-        {
-            return FacingDirection.right;
-        }
-
+        return currentdirection;
         //if (faceDirection != 0)
         //{
         //    transform.localScale = new Vector3(faceDirection, 1, 1);
